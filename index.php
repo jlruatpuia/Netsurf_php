4a1fa45991506119bbb811a7a4985a7453f86fdd
jlruatpuia <jlruatpuia@gmail.com>
2016-02-21 23:08:53 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+include 'layout/temp_start.php';
+include 'layout/header.php';
+?>
+<section id="middle">
+
+
+    <!-- page title -->
+    <header id="page-header">
+        <h1>Blank Page</h1>
+        <ol class="breadcrumb">
+            <li><a href="#">Pages</a></li>
+            <li class="active">Blank Page</li>
+        </ol>
+    </header>
+    <!-- /page title -->
+
+
+    <div id="content" class="padding-20">
+
+        content here...
+
+    </div>
+</section>
+<!-- /MIDDLE -->
+<?php
+include 'layout/footer.php';
+include 'layout/temp_end.php';
+?>
---
+++ layout/footer.php
@@ -0,0 +1,11 @@
+
+</div>
+
+
+
+
+<!-- JAVASCRIPT FILES -->
+<script type="text/javascript">var plugin_path = 'assets/plugins/';</script>
+<script type="text/javascript" src="assets/plugins/jquery/jquery-2.1.4.min.js"></script>
+<script type="text/javascript" src="assets/js/app.js"></script>
+
